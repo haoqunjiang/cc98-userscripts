602c30f66d481453a812d9114632d9e407ff4895
Haoqun Jiang <sodazju@gmail.com>
2014-07-20 03:43:50 +0800

    fix define error in TamperMonkey

--- cc98-blacklist/lib/modular.js
+++ cc98-blacklist/lib/modular.js
@@ -1,11 +1,12 @@
 // simple helper functions to help write modular JavaScript
 // from: http://leechan.me/?p=1241
 // modified by soda<sodazju@gmail.com>
-(function(global) {
+// TamperMonkey 中似乎无法注入全局名称空间，所以就直接定义全局变量了
+(function() {
     var modules = {}, cached = {};
 
     // @usage: define('id', function(exports, module) {});
-    global.define = function(id, func) {
+    define = function(id, func) {
         modules[id] = {};
         if (func instanceof Function) {
             modules[id].factory = func;
@@ -15,11 +16,11 @@
     };
 
     // @usage: var a = require('id');
-    global.require = function(id) {
+    require = function(id) {
         if (cached[id]) {
             return cached[id];
         } else {
             return cached[id] = (modules[id].exports || modules[id].factory(modules[id].exports = {}, modules[id]) || modules[id].exports);
         }
     };
-})(window); // using `this` will cause error in TamperMonkey (that is, the functions are not registered in the global namespace)
+})();
