baf45dd530ded8a464c3917946685d5302ebf27c
Haoqun Jiang <sodazju@gmail.com>
2014-07-20 03:45:33 +0800

    rebuild

--- cc98-blacklist/cc98-blacklist.user.js
+++ cc98-blacklist/cc98-blacklist.user.js
@@ -302,11 +302,12 @@
 // simple helper functions to help write modular JavaScript
 // from: http://leechan.me/?p=1241
 // modified by soda<sodazju@gmail.com>
-(function(global) {
+// TamperMonkey 中似乎无法注入全局名称空间，所以就直接定义全局变量了
+(function() {
     var modules = {}, cached = {};
 
     // @usage: define('id', function(exports, module) {});
-    global.define = function(id, func) {
+    define = function(id, func) {
         modules[id] = {};
         if (func instanceof Function) {
             modules[id].factory = func;
@@ -316,14 +317,14 @@
     };
 
     // @usage: var a = require('id');
-    global.require = function(id) {
+    require = function(id) {
         if (cached[id]) {
             return cached[id];
         } else {
             return cached[id] = (modules[id].exports || modules[id].factory(modules[id].exports = {}, modules[id]) || modules[id].exports);
         }
     };
-})(window); // using `this` will cause error in TamperMonkey (that is, the functions are not registered in the global namespace)
+})();
 
 
 // 本项目中用了自定义的 define 和 require 函数
